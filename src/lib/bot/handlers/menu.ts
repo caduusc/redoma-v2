f8b560b0075f7d5bd0eda88fc52dba6893d0235e
caduusc <carloscaetano7806@gmail.com>
2026-07-12 15:46:27 -0300

    feat: correct wording in menu options for clarity

--- src/lib/bot/handlers/menu.ts
+++ src/lib/bot/handlers/menu.ts
@@ -1,5 +1,5 @@
 import { User } from '@/lib/db/users';
 export async function handleMenu({ user }: { user: User }): Promise<string> {
   void user;
-  return `*Menu Redoma* 🌱\n\ Envie uma das opções abaixo:\n• *APOIE a uma instituição*: É só enviar o link do produto que deseja comprar \n• *PONTOS* para ver seus Impact Points\n• *TROCAR:* para mudar a instituição que está apoiando\n• *PARAR* para não receber mensagens de marketing\n• *AJUDA* para entender como funciona`;
+  return `*Menu Redoma* 🌱\n\ Envie uma das opções abaixo:\n• *APOIE a uma instituição*: É só enviar um link do produto que deseja comprar \n• *PONTOS* para ver seus Impact Points\n• *TROCAR:* para mudar a instituição que está apoiando\n• *PARAR* para não receber mensagens de marketing\n• *AJUDA* para entender como funciona`;
 }
